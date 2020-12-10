79a38c316f12c1e30397ed3d72301682b449c47d
sarah-svg <kjskillen@gmail.com>
2020-12-10 14:03:08 -0800

    test one Post books

--- _test_/app.test.js
+++ _test_/app.test.js
@@ -4,3 +4,30 @@ const request = require('supertest');
 
 const app = require('../lib/app');
 const pool = require('../lib/utils/pool');
+
+
+
+describe('app endpoints are correct', () => {
+  beforeEach(() => {
+    return pool.query(fs.readFileSync(`${__dirname}/../sql/setup.sql`, 'utf-8'));
+
+  });
+
+  afterAll(() => {
+    return pool.end();
+  });
+
+  it('creates a new via POST', async() => {
+    const res = await request(app)
+      .post('/api/v1/author')
+      .send({
+        author: 'ben',
+        title: 'jerry'
+
+      });
+
+    expect(res.body).toEqual({ id: '1',  author: 'ben',
+      title: 'jerry' });
+  });
+
+});
