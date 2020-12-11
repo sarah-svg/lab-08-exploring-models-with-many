752dc96fdf0b7ef0fd7509b1a637faed7cf6ab4d
sarah-svg <kjskillen@gmail.com>
2020-12-10 17:56:56 -0800

    joined

--- _test_/app.test.js
+++ _test_/app.test.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 
 const request = require('supertest');
 
+
 const app = require('../lib/app');
 const Author = require('../lib/models/author');
 const Books = require('../lib/models/whatever');
@@ -23,13 +24,13 @@ describe('app endpoints are correct', () => {
     const res = await request(app)
       .post('/api/v1/books')
       .send({
-        author: 'ben',
+     
         title: 'jerry'
 
       });
 
     expect(res.body).toEqual({
-      id: '1', author: 'ben',
+      id: '1',
       title: 'jerry'
     });
   });
@@ -38,136 +39,163 @@ describe('app endpoints are correct', () => {
     const res = await request(app)
       .post('/api/v1/authors')
       .send({
-        bio: 'ben',
-        author_id: 'jerry'
+        author: 'ben',
+       
 
       });
 
     expect(res.body).toEqual({
-      id: '1', bio: 'ben',
-      author_id: 'jerry'
+      id: '1', author: 'ben',
+  
     });
   });
   ////////////////////////get book by id
   it('finds a book by id via GET', async() => {
-    const book = await Books.insert({ title: 'hot', author: 'dry' });
+    const book = await Books.insert({ title: 'hot' });
 
     const response = await request(app)
       .get(`/api/v1/books/${book.id}`);
 
     expect(response.body).toEqual(book);
   });
-
   it('finds a author by id via GET', async() => {
     const author = await Author.insert({      bio: 'ben',
-      author_id: 'jerry' });
+    });
 
     const response = await request(app)
       .get(`/api/v1/authors/${author.id}`);
 
     expect(response.body).toEqual(author);
   });
-  ///////////////get all authors and books
-  it('finds all books via GET', async() => {
-    const book = await Promise.all([
-      {
-        author: 'ben',
-        title: 'jerry'
-      },
-      {
-        author: 'ben',
-        title: 'jerry'
-      },
-    ].map(book => Books.insert(book)));
-    const res = await request(app)
-      .get('/api/v1/books');
-
-    expect(res.body).toEqual(expect.arrayContaining(book));
-    expect(res.body).toHaveLength(book.length);
-  });
-
-  it('finds all author via GET', async() => {
-    const author = await Promise.all([
-      {
-        id: '1', bio: 'ben',
-        author_id: 'jerry'
-      },
-      {
-        id: '1', bio: 'ben',
-        author_id: 'jerry'
-      },
+  /////////////
+  it('finds a book by id via GET', async() => {
+    await Promise.all ([
+      { author: 'ryan' },
+      { author: 'Dan' },
+      { author: 'jake' },
+      { author: 'langston' },
     ].map(author => Author.insert(author)));
+        
+    const newBook = await Books.insert({
+      title: 'hey',
+      authors: ['ryan', 'Dan', 'langston']
+    });
     const res = await request(app)
-      .get('/api/v1/authors');
+      .get(`/api/v1/books/${newBook.id}`);
+    expect(res.body).toEqual({ ...newBook, authors: ['ryan', 'Dan', 'langston'] });
 
-    expect(res.body).toEqual(expect.arrayContaining(author));
-    expect(res.body).toHaveLength(author.length);
+
+
   });
-  //////////////////updates books and authors
-  it('updates a books via PUT', async() => {
-    const book = await Books.insert({
-      author: 'barry',
-      title: 'jerry'
-    });
-
-    const res = await request(app)
-      .put(`/api/v1/books/${book.id}`)
-      .send({
-        author: 'ben',
-        title: 'jerry'
-      });
-
-    expect(res.body).toEqual({
-      id: '1',
-      author: 'ben',
-      title: 'jerry'
-    });
-  });
-  it('updates a author via PUT', async() => {
-    const author = await Author.insert({
-      bio: 'ben',
-      author_id: 'jerry j'
-    });
-
-    const res = await request(app)
-      .put(`/api/v1/authors/${author.id}`)
-      .send({
-        bio: 'ben',
-        author_id: 'jerry'
-      });
-
-    expect(res.body).toEqual({
-      id: '1',
-      bio: 'ben',
-      author_id: 'jerry'
-    });
-  });
-
-  ///////////////////////////////////////////delete
-  it('deletes a book via DELETE', async() => {
-    const books = await Books.insert({
-      author: 'ben',
-      title: 'jerry'
-    });
-
-    const res = await request(app)
-      .delete(`/api/v1/books/${books.id}`);
-
-    expect(res.body).toEqual(books);
-  });
-  it('deletes a author via DELETE', async() => {
-    const author = await Author.insert({
-      bio: 'ben',
-      author_id: 'jerry'
-    });
-
-    const res = await request(app)
-      .delete(`/api/v1/authors/${author.id}`);
-
-    expect(res.body).toEqual(author);
-  });
-
-
 
 
 });
+
+
+
+
+///////////////get all authors and books
+
+
+it('finds all books via GET', async() => {
+  const book = await Promise.all([
+    {
+ 
+      title: 'jerry'
+    },
+    {
+   
+      title: 'jerry'
+    },
+  ].map(book => Books.insert(book)));
+  const res = await request(app)
+    .get('/api/v1/books');
+
+  expect(res.body).toEqual(expect.arrayContaining(book));
+  expect(res.body).toHaveLength(book.length);
+});
+
+it('finds all author via GET', async() => {
+  const author = await Promise.all([
+    {
+      id: '1', author: 'ben',
+     
+    },
+    {
+      id: '1', author: 'ben',
+     
+    },
+  ].map(author => Author.insert(author)));
+  const res = await request(app)
+    .get('/api/v1/authors');
+
+  expect(res.body).toEqual(expect.arrayContaining(author));
+  expect(res.body).toHaveLength(author.length);
+});
+//////////////////updates books and authors
+it('updates a books via PUT', async() => {
+  const book = await Books.insert({
+
+    title: 'jerry'
+  });
+
+  const res = await request(app)
+    .put(`/api/v1/books/${book.id}`)
+    .send({
+     
+      title: 'jerry'
+    });
+
+  expect(res.body).toEqual({
+    id: '1',
+ 
+    title: 'jerry'
+  });
+});
+it('updates a author via PUT', async() => {
+  const author = await Author.insert({
+    author: 'ben',
+ 
+  });
+
+  const res = await request(app)
+    .put(`/api/v1/authors/${author.id}`)
+    .send({
+      author: 'ben',
+  
+    });
+
+  expect(res.body).toEqual({
+    id: '1',
+    author: 'ben',
+  
+  });
+});
+
+///////////////////////////////////////////delete
+it('deletes a book via DELETE', async() => {
+  const books = await Books.insert({
+  
+    title: 'jerry'
+  });
+
+  const res = await request(app)
+    .delete(`/api/v1/books/${books.id}`);
+
+  expect(res.body).toEqual(books);
+});
+it('deletes a author via DELETE', async() => {
+  const author = await Author.insert({
+    author: 'ben',
+  
+  });
+
+  const res = await request(app)
+    .delete(`/api/v1/authors/${author.id}`);
+
+  expect(res.body).toEqual(author);
+});
+
+
+
+
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,21 +1,27 @@
 DROP TABLE IF EXISTS books CASCADE;
-DROP TABLE IF EXISTS authors;
+DROP TABLE IF EXISTS authors CASCADE;
+DROP TABLE If EXISTS author_books;
+
+
+
 
 CREATE TABLE books (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    title TEXT NOT NULL,
-    author TEXT NOT NULL
+    title TEXT NOT NULL
+   
 
 
 );
--- CREATE TABLE weather (
---     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
---      bio TEXT NOT NULL,
---      author_id  BIGINT REFERENCES flowers(id)
--- );
+
 
 CREATE TABLE authors (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    bio TEXT NOT NULL,
-    author_id  TEXT NOT NULL
+    author TEXT NOT NULL
+  
+);
+
+CREATE TABLE author_books (
+    author_id BIGINT REFERENCES authors(id),
+    books_id BIGINT REFERENCES books(id),
+    PRIMARY KEY(author_id, books_id)
 );
